ef575ffb8ceb027ce1529817402e186a1c006493
magruribragimovvich-oss <magruribragimovvich@gmail.com>
2026-02-19 12:59:30 +0500

    updLessonOne

--- app.js
+++ app.js
@@ -64,3 +64,86 @@
 
 // let sum = Number(a) + Number(b) + d + e;
 // console.log(sum) // 45.14
+
+
+//CHALLANGE 1 LESSON 2
+// const n = Number(prompt("Enter a number:"));
+// let evenCount = 0;
+// let oddCount = 0;
+
+// for (let i = 1; i <= n; i++) {
+//   if (i % 2 === 0) {
+//     evenCount++;
+//   } else {
+//     oddCount++;
+//   }
+// }
+// console.log("Even numbers:", evenCount);
+// console.log("Odd numbers:", oddCount);
+
+//CHALLANGE 2 LESSON 2
+// let m = Number(prompt("Enter a number:"));
+// let reversed = 0;
+
+// while (m > 0) {
+//   let lastDigit = m % 10;                  
+//   reversed = reversed * 10 + lastDigit;    
+//   m = (m - lastDigit) / 10;                
+// }
+// console.log("Reversed number:", reversed);
+
+
+//CHALLANGE 3 LESSON 2
+// let a = Number(prompt("Enter a number:"));
+// let largest = 0;
+
+// if (a === 0) {
+//   largest = 0;
+// } else {
+//   while (a > 0) {
+//     let digit = a % 10;          
+
+//     if (digit > largest) {
+//       largest = digit;          
+//     }
+
+//     a= (a - digit) / 10;        
+//   }
+// }
+
+// console.log("Largest digit:", largest);
+
+
+//CHALLANGE 4 LESSON 2
+// let b = Number(prompt("Enter a number:"));
+
+// let sum = 0;
+// if (b === 0) {
+//   sum = 0;
+// } else {
+//   while (b > 0) {
+//     let digit = n % 10;        
+//     sum = sum + digit;         
+//     b = (b - digit) / 10;      
+//   }
+// }
+// console.log("Sum of digits:", sum);
+
+
+//CHALLANGE 5 LESSON 2
+// let c = Number(prompt("Enter a number:"));
+// let count = 0;
+
+// if (c === 0) {
+//   count = 1;
+// } else {
+//   while (c > 0) {
+//     c = Math.floor(c / 10);
+//     count++;
+//   }
+// }
+// console.log("Number of digits:", count);
+
+
+
+
