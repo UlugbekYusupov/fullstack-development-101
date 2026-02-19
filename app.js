bf51e5dbd264c5f872b0ad98982f9166882ea827
Sanatbek Bokijonov <bokijonovs@gmail.com>
2026-02-19 13:17:27 +0500

    lesson 2 completed

--- app.js
+++ app.js
@@ -1,53 +1,88 @@
-console.log('5' + 5)
-console.log('5' - 5)
-console.log('5' * 5)
-console.log('10' / 5)
-console.log(5 + true)
-console.log(5 - true)
-console.log('5' + true)
-console.log(5 + null)
-console.log(5 + undefined)
+// console.log('5' + 5)
+// console.log('5' - 5)
+// console.log('5' * 5)
+// console.log('10' / 5)
+// console.log(5 + true)
+// console.log(5 - true)
+// console.log('5' + true)
+// console.log(5 + null)
+// console.log(5 + undefined)
 
 
-console.log(Number('200') + 50)
-console.log(String(200) + "years old")
-if ('false') {
-    console.log('True')
-}
-else{
-    console.log('False')
-}
+// console.log(Number('200') + 50)
+// console.log(String(200) + "years old")
+// if ('false') {
+//     console.log('True')
+// }
+// else{
+//     console.log('False')
+// }
 
-console.log(Boolean(0))
-
-
-const temperature = prompt("Enter the tempeature in Celsius: ")
-const fahrenheit = (temperature * 9 / 5) + 32
-console.log(`${temperature}°C is equal to ${fahrenheit}°F`)
-
-let budget = prompt("Enter your budget: ")
-for (let i = 0; i < 3; i++) {
-    if (budget < 0) {
-        alert("You have exceeded your budget!")
-    }
-    budget -= Number(prompt("Enter an expense: "))
-}
-budget = budget.toFixed(2)
-console.log(`Remaining budget: ${budget}`)
+// console.log(Boolean(0))
 
 
+// const temperature = prompt("Enter the tempeature in Celsius: ")
+// const fahrenheit = (temperature * 9 / 5) + 32
+// console.log(`${temperature}°C is equal to ${fahrenheit}°F`)
 
-console.log(!!"false" == !!"true")
-console.log(0||"Javascript")
-console.log(" " && 100)
-console.log(null ?? "Fallback")
-console.log(undefined ?? "Default value")
+// let budget = prompt("Enter your budget: ")
+// for (let i = 0; i < 3; i++) {
+//     if (budget < 0) {
+//         alert("You have exceeded your budget!")
+//     }
+//     budget -= Number(prompt("Enter an expense: "))
+// }
+// budget = budget.toFixed(2)
+// console.log(`Remaining budget: ${budget}`)
 
-let a = "42"
-let b = "3.14"
-let c = "Hello"
-let d = false
-let e = null
 
-let sum = Number(a) + Number(b) + Number(d) + Number(e)
-console.log(sum)
+
+// console.log(!!"false" == !!"true")
+// console.log(0||"Javascript")
+// console.log(" " && 100)
+// console.log(null ?? "Fallback")
+// console.log(undefined ?? "Default value")
+
+// let a = "42"
+// let b = "3.14"
+// let c = "Hello"
+// let d = false
+// let e = null
+
+// let sum = Number(a) + Number(b) + Number(d) + Number(e)
+// console.log(sum)
+
+// let num = Number(prompt("Enter a number: "))
+// let sum_odd = 0
+// let sum_even = 0
+// for (let i = 1; i <= num; i+=2) {
+//     sum_odd += 1
+// }
+// for (let i = 2; i <= num; i+=2) {
+//     sum_even += 1
+// }
+// console.log(`Sum of odd numbers: ${sum_odd}`)
+// console.log(`Sum of even numbers: ${sum_even}`)
+
+// let num = Number(prompt("Enter a number: "))
+// let reversed = 0
+// while (num > 0) {
+//     let digit = num % 10
+//     reversed = (reversed * 10) + digit
+//     num = Math.floor(num / 10)
+// }
+// console.log(`Reversed number: ${reversed}`)
+
+// let num = Number(prompt("Enter a number: "))
+// let largest = 0
+// while (num > 0) {
+//     let digit = num % 10
+//     if (digit > largest) {
+//         largest = digit
+//     }
+//     num = Math.floor(num / 10)
+// }
+// console.log(`Largest digit: ${largest}`)
+
+
+
