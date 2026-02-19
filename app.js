c469c28afe41997ef3311d6337da9f910867a9c8
Shahzod Omonboyev Mac <shahzodomonboyev1@gmail.com>
2026-02-19 12:10:49 +0500

    first lesson doing some tasks exit

---
+++ app.js
@@ -0,0 +1,57 @@
+console.log("5" + 5)
+console.log("5" - 5)
+console.log("5" * 5)
+console.log("10" / 2)
+console.log(5 + true)
+console.log('10' - true)
+console.log("5" + true)
+console.log(5 + null)
+console.log(5 + undefined)
+
+// task2
+console.log(Number("200") + 50)
+console.log(String(25) + "years old")
+if ("false") {
+    console.log("true")
+} else {
+    console.log("false")
+}
+console.log(10 - true)
+
+// task 3
+const temp = 30
+console.log((temp * 9 / 5) + 32)
+console.log(temp + "C" + "=" + ((temp * 9 / 5) + 32) + "F")
+ask_c = prompt("Enter temperature in celsius")
+aler(`Fahrenheit: ${((ask_c * 9 / 5) + 32)}`)
+
+// task 4
+let user_budget;
+user_budget = Number(prompt("Enter your budget"))
+let expenses = 0
+for (let i = 0; i < 3; i++) {
+    expenses += Number(prompt("Enter your expenses"))
+}
+check = user_budget - expenses
+if (check < 0) {
+    alert(`You are over budget by ${check.toFixed()}`)
+} else {
+    alert(`You are not over budget by ${check.toFixed()}`)
+}
+// task 5
+console.log("Task 5")
+console.log(!!"false" == !!"true");
+console.log(0 || "Javascript")
+console.log(" " && 100)
+console.log(null ?? "Fallback")
+console.log(undefined ?? "default")
+
+// task 6
+let a  ="42"
+let b = "3.14"
+let c = 'hello'
+let d = false
+let e  = null
+
+let sum = Number(a) + Number(b)+Number( d) +Number (e)
+console.log(sum)
