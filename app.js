81a8b6c73c1abd66690218a123c6b2081892cfaf
Fayozbek <fayozbektoychiboyev05@gmail.com>
2026-02-19 13:02:33 +0500

    updLesson1

---
+++ app.js
@@ -0,0 +1,79 @@
+// Lesson 1 data types and type conversion
+
+
+// Challenge 1
+// console.log("5"+5); //?
+// console.log("5"-5); //?
+// console.log("5"*2); //?
+// console.log("10"/2); //?
+// console.log(5 + true); //?
+// console.log("10"-true); //?
+// console.log("5"+true); //?
+// console.log(5+null); //?
+// console.log(5 + undefined); //?
+
+
+
+// Challenge 2
+//Task 1
+// console.log("200"+50); //?
+// console.log(String(25)+ "years old"); //?
+//Task 2
+// let a = "false";
+// if (a== String) {
+//   console.log("True");
+// }
+// else {
+//   console.log("False");
+// }
+//Task 3
+// console.log("10" - true)
+
+
+
+// Challenge 3
+// Task 1
+// const TemperatureCelsius = 30;
+// Task 2
+// const temperatureFahrenheit = (TemperatureCelsius * 9/5) + 32;
+// console.log(temperatureFahrenheit); 
+// Task 3
+// console.log("The temperature in Fahrenheit is: " + temperatureFahrenheit + "°F");
+// Task 4
+// const temperatureCelsius = 25;
+// temperatureCelsius = 30; // error
+// Task 5
+// const celsiusStr = prompt("Enter temperature in Celsius:");
+// const celsius = Number(celsiusStr);
+
+
+// Challenge 4
+// Task 1
+// let budget;
+// Task 2
+// budget = Number(prompt("Enter your total budget:"));
+// // Task 3
+// const exp1 = Number(prompt("Enter expense 1:"));
+// const exp2 = Number(prompt("Enter expense 2:"));
+// const exp3 = Number(prompt("Enter expense 3:"));
+// // Task 4
+// const remainingBudget = budget - (exp1 + exp2 + exp3);
+// // Task 5
+// if (remainingBudget < 0) {
+//   console.log("Warning: You have overspent!");
+// }
+// Task 6
+// console.log(remainingBudget.toFixed(2));
+
+// Teacher I had used toFixed(2) to round the remaining budget to 2 decimal places, And I learn it from the internet, I hope you don't mind.
+
+
+// Challenge 5
+//Challenge 6
+
+
+
+
+//Lesson 2 operators and expressions
+
+// Challenge 1
