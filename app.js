98c64d46a0d36cb7a29b2964d2529698fa0ce8c3
Aziz <usanovazizsh1@gmail.com>
2026-02-19 13:56:47 +0500

    Operators

--- app.js
+++ app.js
@@ -1,56 +1,102 @@
-// task1
-console.log("5"+5); //55
-console.log("5"-5); //0
-console.log("5"*"2"); //10
-console.log("5"/2); //2.5
-console.log(5+true); //6
-console.log("10"-true); //9
-console.log("5"+true); //5true
-console.log(5+null); //5
-console.log(5+undefined); //NaN
+//-----------------------------------------------1-DARS-----------------------------------------
+// // task1
+// console.log("5"+5); //55
+// console.log("5"-5); //0
+// console.log("5"*"2"); //10
+// console.log("5"/2); //2.5
+// console.log(5+true); //6
+// console.log("10"-true); //9
+// console.log("5"+true); //5true
+// console.log(5+null); //5
+// console.log(5+undefined); //NaN
 
-// task2
-let x="200"
-let result=Number(x)+500
-console.log(result)
+// // task2
+// let x="200"
+// let result=Number(x)+500
+// console.log(result)
 
-let y=25
-let yResult=String(y)+ ' years old'
-console.log(yResult)
+// let y=25
+// let yResult=String(y)+ ' years old'
+// console.log(yResult)
 
-// task3
-const temp=Number(prompt('Enter a temperature in Celcius'))
-const tempResult=temp*9/5+32
-console.log(temp)
-console.log(tempResult)
+// // task3
+// const temp=Number(prompt('Enter a temperature in Celcius'))
+// const tempResult=temp*9/5+32
+// console.log(temp)
+// console.log(tempResult)
 
-// task4
-let budget=Number(prompt('Your amount of budget'))
-let expence1=Number(prompt('1st expence'))
-let expence2=Number(prompt('2nd expence'))
-let expence3=Number(prompt('3rd expence'))
-const budgetResult=budget-expence1-expence2-expence3
-console.log(budgetResult)
+// // task4
+// let budget=Number(prompt('Your amount of budget'))
+// let expence1=Number(prompt('1st expence'))
+// let expence2=Number(prompt('2nd expence'))
+// let expence3=Number(prompt('3rd expence'))
+// const budgetResult=budget-expence1-expence2-expence3
+// console.log(budgetResult)
 
-// task5
-console.log(!!"false"==!!"true") // true
-console.log(0||"JavaScript") // JavaScript
-console.log(" " && 100) // 100
-console.log(null ?? "Fallback") // Fallback
-console.log(undefined ?? "Default Value") // Default Value
+// // task5
+// console.log(!!"false"==!!"true") // true
+// console.log(0||"JavaScript") // JavaScript
+// console.log(" " && 100) // 100
+// console.log(null ?? "Fallback") // Fallback
+// console.log(undefined ?? "Default Value") // Default Value
 
-// task6
-let a = "42";
-let b = "3.14";
-let c = "hello";
-let d = false;
-let e = null;
+// // task6
+// let a = "42";
+// let b = "3.14";
+// let c = "hello";
+// let d = false;
+// let e = null;
 
-a=Number(a)
-b=Number(b)
-d=Number(d)
-e=Number(e)
+// a=Number(a)
+// b=Number(b)
+// d=Number(d)
+// e=Number(e)
 
-let sum = a+b+d+e
+// let sum = a+b+d+e
 
-console.log(sum)
+// console.log(sum)
+
+//-----------------------------------------------2-DARS-----------------------------------------
+
+//task1
+
+let givenNum1=prompt('Enter a random number between 10-40')
+
+let evenCount = 0;
+let oddCount = 0;
+
+for (let num = 1; num <= givenNum1; num++) {
+    if (num % 2 === 0) {
+        evenCount++;
+    } else {
+        oddCount++;
+    }
+}
+
+console.log("Even numbers count:", evenCount);
+console.log("Odd numbers count:", oddCount);
+
+//task2
+
+let givenNum2=prompt('Enter a number, i will return')
+let reverse=0;
+while(givenNum2>0){
+    let digit=givenNum2%10;
+    givenNum2=Math.floor(givenNum2/10);
+    reverse=reverse*10+digit;
+}
+console.log(reverse)
+
+//task3
+
+// Challenge #3
+let num = prompt('Enter a long number. I will find a biggest digit');
+let largest = 0;
+while (num > 0) {
+  let digit = num % 10;
+  if (digit > largest) {
+    largest = digit;
+  }
+  num = Math.floor(num / 10);
+}
+console.log("Largest digit:", largest);
