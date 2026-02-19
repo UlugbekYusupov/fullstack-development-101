8fb01b60d125cff6aedfc6526b660830ffd871ee
magruribragimovvich-oss <magruribragimovvich@gmail.com>
2026-02-19 12:02:00 +0500

    new

---
+++ app.js
@@ -0,0 +1,66 @@
+//CHALLANGE 1
+// console.log('5' + 5); // 55
+// console.log('5' - 5); // 0
+// console.log('5' * '2'); //10
+// console.log('10' / 2); // 5
+// console.log(5 + true); // 6
+// console.log('10' - true); // 9
+// console.log('5' + true); // 5true
+// console.log(5 + null); // 5
+// console.log(5 + undefined); //NaN
+
+// console.log(Number("200") + 50);
+// console.log(String(25) + " years old")
+
+//CHALLANGE 2
+// a = 'false'
+
+// if (a){
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+// }
+
+// console.log('10' - true)
+// // JavaScript converts '10' automaticly because of the minus sign.
+
+//CHALLANGE 3
+// const temperature = prompt('Enter todays temperature: ');
+// const F = (temperature*9/5) + 32;
+// console.log(`In Fahrenheit it is ${F} farenheit`);
+
+
+//CHALLANGE 4
+// let firstBudget = Number(prompt('Enter your total budget: '));
+
+// let firstExpense = String(prompt('Enter your first expense: '));
+// let secondExpense = String(prompt('Enter your second expense: '));
+// let thirdExpense = String(prompt('Enter your third expense: '));
+
+// let newBudget = firstBudget - (Number(firstExpense) + Number(secondExpense) + Number(thirdExpense));
+// if (newBudget < 0){
+//     alert('You overspent your mnoey!')
+// }
+// else{
+//     alert('your new budget' + ' ' +  newBudget + '$')
+//}
+
+
+//CHALLANGE 5
+// console.log(!!'false' == !!"true"); // true
+// console.log(0 || "JavaScript"); // JavaScript
+// console.log(" " && 100); // 100
+// console.log(null ?? "Fallback") // Fallback;
+// console.log(undefined ?? "Default Value"); // Default Value
+
+
+//CHALLANGE 6
+// let a = '42';
+// let b = '3.14';
+// let c = 'hello';
+// let d = false;
+// let e = null;
+
+// let sum = a+b+c+d+e;
+// console.log(sum) // 423.14hellofalsenull
