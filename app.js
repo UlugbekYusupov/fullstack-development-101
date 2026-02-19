e2cccafb3de41b84da782fe6b6adb68f34720eee
Doston <abdurazzoqovdoston75@gmail.com>
2026-02-19 12:11:50 +0500

    Lesson #1

---
+++ app.js
@@ -0,0 +1,30 @@
+console.log("I'm HTML engineer");
+
+console.log("5" + 5); // 7
+console.log("5" - 5); // ?
+console.log("5" * "2"); // ?
+console.log("10" / 2); // 7
+console.log(5 + true); // ?
+console.log("10" - true); // ?
+console.log("5" + true); // ?
+console.log(5 + null); // ?
+console.log(5 + undefined); // 7
+
+// Other tasks
+
+console.log("200" * 1 + 50);
+console.log(25 + " year old");
+console.log("false" == true);
+console.log("10" - false);
+
+// Yet another group of tasks
+
+// const temp = prompt("Enter ℃ value: ");
+// var converted = (temp * 9) / 5 + 32;
+// alert(temp + "℃ to ℉ is: " + converted);
+
+
+var budget = 0
+
+var budget = prompt("Enter your budget: ")
+
