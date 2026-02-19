6c1f84b239cdcb268ebf45f242dbb17809a8c1e2
Bexzod <bexzodaxmadjonovx@gmail.com>
2026-02-19 12:09:23 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+// console.log("5" + 5);
+// console.log("5" - 5);
+// console.log("5" * "2");
+// console.log("10" / 2);
+// console.log(5 + true);
+// console.log("10" - true)
+// console.log("5" + true)
+// console.log(5 + null);
+// console.log(5 + undefined);
+
+// challenge 2
+// str = "200"
+// str = 200
+// console.log(str + 50)
+// //
+// num = 25
+// console.log(num + " years old")
+// //
+// console.log("false" == Boolean)
+// //
+// console.log("10" - true)
+//
+// Challenge 3: Temperature Converter
+// const tmp = prompt()
+// const fahrenheitTmp = (tmp * (9/5))+32;
+// console.log(tmp, fahrenheitTmp);
+
+// tmp = 20;
+//
+
+// Challenge 4: Budget Calculator
+// let totalBudget = prompt("Enter your Budget: ");
+// totalBudget = parseInt(totalBudget);
+// let expenses = prompt("Enter you expenses: ");
+//
+// Challenge 5: Logical Coercion Trickery
+// console.log(!!"false" == !!"true");
+// console.log(0 || "Javascript");
+// console.log(" " ?? "Fallback");
+// console.log(null ?? "Fallback");
+// console.log(undefined ?? "Default Value");
+//
+// Challenge 6: Convert Values Properly
+// let a = "42";
+// let b = "3.14";
+// let c = "hello";
+// let d = false
+// let e = null;
+// a = parseInt(a);
+// b = parseFloat(b);
+// // Convert all to numbers and add them together
+// let sum = a + b + d + e;
+
+// console.log(sum); // Fix this so the result is a valid number
