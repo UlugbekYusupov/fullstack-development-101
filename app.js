5291a61b0291820d6d6ed2267bd3ba8264a36caf
Ismingiz <email@example.com>
2026-02-24 13:04:58 +0500

    Control Structures lesson's practices

--- app.js
+++ app.js
@@ -131,15 +131,158 @@
 
 // console.log(sum);
 
-// Challange 5
+// // Challange 5
 
-let number3 = Number(prompt("Enter a number: "));
-let count = 0;
+// let number3 = Number(prompt("Enter a number: "));
+// let count = 0;
 
-while (number3 > 0) {
-    let digit = number3 % 10;
-    count ++;
-    number3 = Math.floor(number3 / 10);
-};
+// while (number3 > 0) {
+//     let digit = number3 % 10;
+//     count ++;
+//     number3 = Math.floor(number3 / 10);
+// };
 
-console.log(count);
+// console.log(count);
+
+// // // Lesson 2 Lecture 4 || 24.02.2026
+
+// // for loop
+// for(let i  = 10; i < 10; i++) {
+//     console.log(i);
+// };
+
+// // while loop
+// let i = 1;
+// while (i < 10) {
+//     console.log(i);
+//     i++;
+// };
+
+// // do while loop
+// let a = 0;
+// do {
+//     console.log(a);
+//     a++;
+// } while (a < 10);
+
+// const user = {
+//     name: "otabek",
+//     age: 19,
+//     country: "Uzbekistan"
+// };
+
+// // for...in (object uchun)
+// for (const key in user) {
+//     const element = user[key];
+//     console.log(element);
+// }
+
+// // for...of (array uchun)
+// for (const element of Object.values(user)) {
+//     console.log(element);
+// }
+
+// Practices
+
+// // Practise 1
+
+// const num1 = 23;
+// const num2 = 83;
+
+// if (num1 > num2) {
+//     console.log(`The largest number is ${num1}`);
+// } else {
+//     console.log(`The largest number is ${num2}`);
+// }
+
+// // Practice 2
+
+// const numbers = [3, -7, 2];
+
+// let i = 0;
+// let found = false;
+
+// while (i < numbers.length) {
+//     if (numbers[i] < 0) {
+//         alert(`The sign is ${numbers[i]}`);
+//         found = true;
+//         break;
+//     }
+//     i++;
+// }
+
+// if (!found) {
+//     alert("We have not a signed number");
+// }
+
+// // Practice 3
+
+// const numbers = [0, -1, 4];
+// let sortedNumbers = numbers.sort();
+// let reversedNumbers = sortedNumbers.reverse();
+// alert(reversedNumbers);
+
+// // Practice 4
+
+// for (let i = 0; i <= 15; i++) {
+//     if (i % 2 === 0) {
+//         console.log(`${i} is even`);
+//     } else {
+//         console.log(`${i} is odd`);
+//     };
+// };
+
+// // Practice 5
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//         console.log("Fizz");
+//     } else if (i % 3 === 0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log(i);
+//     };
+// };
+
+// // Practice 6
+
+// let number = prompt("Enter a number");
+// let armstrong = 0;
+// const daraja = number.length;
+
+// let  i = 0;
+
+// while (i < number.length) {
+//     let digit = Number(number[i]);
+//     armstrong += digit ** daraja;
+//     i++;
+// }
+
+// if (armstrong === Number(number)) {
+//     console.log(`The number (${number}) is armstrong`);
+// } else {
+//     console.log(`The number (${number}) is not armstrong`);
+// }
+
+// Practice 7
+
+const marks = {
+    David: 80,
+    Vinoth: 77,
+    Divya: 88,
+    Ishitha: 95,
+    Thomas: 68
+}
+
+let totalMarks = 0;
+let totalStudent = 0;
+
+for (let key in marks) {
+    totalMarks += marks[key];
+    totalStudent++;
+}
+
+let average = totalMarks / totalStudent;
+console.log(`Average mark of this class: ${average}`);
