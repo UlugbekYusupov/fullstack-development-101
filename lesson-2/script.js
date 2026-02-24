734f92ebe98770cad4f7f46b4dcea45e634bfe79
Sardorbek <sardorbek.safarboyev06@gmail.com>
2026-02-24 10:03:23 +0500

    Completed the tasks of Lesson 2 and Lessson 3

---
+++ lesson-2/script.js
@@ -0,0 +1,56 @@
+// CHALLENGE 2
+// Task 1
+// let converted = Number("200")
+// console.log( converted + 50)
+
+// Task 2
+// console.log(25 + " years old")
+
+// Task 3
+// console.log(Boolean("false"))
+
+// Task 4
+// console.log("10" - true)
+// The value of the true is being taken as 1, as the 
+// boolean value can be 0 for false and 1 for true
+
+
+// CHALLENGE 3
+// Task 1
+// const tempC = 25
+
+// Task 2
+// const tempF = (tempC*(9/5)) + 32
+
+// Task 3
+// console.log("In Celciues: " + tempC)
+// console.log("In Farenheit: " + tempF)
+
+// const tempInput = prompt("Enter the temperature in Celcius: ")
+// const tempInputF = (tempInput*(9/5)) + 32
+// console.log("The temperture in Celcius that you entered: " + tempInput) 
+// console.log("This temperature in Farneheit will be: " + tempInputF)
+
+// CHALLENGE 4
+// let totalBudget;
+// totalBudget = parseFloat(prompt("Enter the full budget: "));
+
+// let expense1 = parseFloat(prompt("Enter an expense: "))
+// let expense2 = parseFloat(prompt("Enter an expense: "))
+// let expense3 = parseFloat(prompt("Enter an expense: "))
+// totalBudget = totalBudget - expense1 - expense2 - expense3;
+// if (totalBudget < 0) {
+//     alert("You overspent!!!")
+// } else {
+//     console.log("Your current budget is " + totalBudget.toFixed(2))
+// }
+
+
+// CHALLENGE 6
+// let a = parseFloat("42") || 0;
+// let b = parseFloat("3.14") || 0;
+// let c = parseFloat("hello") || 0;
+// let d = parseFloat(false) || 0;
+// let e = parseFloat(null) || 0;
+// let sum = a + b + c + d + e;
+// console.log("Sum is " + sum)
---
+++ lesson-3/script.js
@@ -0,0 +1,60 @@
+// CHALLENGE 1
+// const num = parseFloat(prompt('Enter a number: '));
+// let evens = 0;
+// let odds = 0;
+// for (let i=1; i < num; i++) {
+//     if (i % 2 == 0) {
+//         evens ++;
+//     } else {
+//         odds ++ ;
+//     }
+// }
+// console.log("Number of evens: " + evens);
+// console.log("Number of odds: " + odds);
+
+// CHALLANGE 2
+// let num = parseInt(prompt("Enter a number: "));
+// let reversed = 0;
+// while (num > 0) {
+//     let lastDigit = num % 10;               
+//     reversed = (reversed * 10) + lastDigit;  
+//     num = Math.floor(num / 10);              
+// }
+
+// console.log("Reversed: " + reversed);
+
+// CHALLENGE 3
+// let num = parseInt(prompt("Enter a number: "));
+// let largest = 0;
+// while (num > 0) {
+//     let lastDigit = num % 10;
+//     if (lastDigit > largest) {
+//         largest = lastDigit;
+//     }               
+//     num = Math.floor(num / 10);              
+// }
+
+// console.log("Largest: " + largest);
+
+// CHALLENGE 4
+// let num = parseInt(prompt("Enter a number: "));
+// let sumofDigits = 0;
+// while (num > 0) {
+//     let lastDigit = num % 10;
+//     sumofDigits += lastDigit;             
+//     num = Math.floor(num / 10);              
+// }
+
+// console.log("Sum of the digits: " + sumofDigits);
+
+
+// CHALLENGE 5 
+// let num = parseInt(prompt("Enter a number: "));
+// let countDigits = 0;
+// while (num > 0) {
+//     let lastDigit = num % 10;
+//     countDigits ++;             
+//     num = Math.floor(num / 10);              
+// }
+
+// console.log("Number of digits: " + countDigits);
