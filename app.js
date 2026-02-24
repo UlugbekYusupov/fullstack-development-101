5f4c45787babf9ca6cc3fe9d4916b87f21b7f82b
UlugbekYusupov <bilmadim.uz@gmail.com>
2026-02-24 12:25:25 +0500

    lecture 4 loops

---
+++ app.js
@@ -0,0 +1,73 @@
+// var a = undefined;
+// console.log(a);
+// var a = 40;
+
+// let user = {
+//   username: "Ulugbek",
+//   age: 30,
+// };
+
+// console.log(
+//   {
+//     username: "Ulugbel",
+//   } ==
+//     {
+//       username: "Ulugbel",
+//     }
+// );
+
+// let number = 1234;
+// let reverse = 0;
+
+// while (number > 0) {
+//   let digit = number % 10;
+//   number = Math.floor(number / 10);
+//   reverse = reverse * 10 + digit;
+// }
+
+// console.log(reverse);
+
+// task 1.1
+
+// let number = Number(prompt("Enter the Number"));
+// let count = 0;
+// while (number > 0) {
+//   number = Math.floor(number / 10);
+//   count++;
+// }
+
+// console.log(count);
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+let i = 0;
+while (i < 10) {
+  console.log(i);
+  i++;
+}
+
+let j = 0;
+do {
+  console.log(j);
+  j++;
+} while (j < 10);
+
+const user = {
+  username: "Ulugbek",
+  age: 30,
+  country: "Uzbekistan",
+};
+
+const keys = Object.keys(user);
+const values = Object.values(user);
+
+for (const key in user) {
+  const element = user[key];
+  console.log(element);
+}
+
+for (const element of keys) {
+  console.log(element);
+}
