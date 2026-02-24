715da61b3bdab8dc633c8175eebcdd14944611ad
Sanatbek Bokijonov <bokijonovs@gmail.com>
2026-02-24 12:55:48 +0500

    feb 24

--- app.js
+++ app.js
@@ -84,5 +84,99 @@
 // }
 // console.log(`Largest digit: ${largest}`)
 
+// feb 24 ====================================================================
+
+// a = Number(prompt("Enter a number: "))
+// b = Number(prompt("Enter another number: "))
+// if (a > b) {
+//     console.log(`${a} is greater than ${b}`)
+// }
+// else if (b > a) {
+//     console.log(`${b} is greater than ${a}`)
+// }
+// else {
+//     console.log("Both numbers are equal")
+// }
+
+// numbers = [2, -7, 4]
+// for (i in numbers) {
+//     if (numbers[i] < 0){
+//         alert("your number is " + numbers[i])
+//     }
+// }
+// let num1 = Number(prompt("Enter first number: "))
+// let num2 = Number(prompt("Enter second number: "))
+// let num3 = Number(prompt("Enter third number: "))
+
+// if (num1 > num2 && num1 > num3) {
+//     console.log(`Largest: ${num1}`)
+// } else if (num2 > num1 && num2 > num3) {
+//     console.log(`Largest: ${num2}`)
+// } else {
+//     console.log(`Largest: ${num3}`)
+// }
+
+// for (let i = 1; i <= 15; i++) {
+//     if (i % 2 === 0) {
+//         console.log(`${i} is even`)
+//     } else {
+//         console.log(`${i} is odd`)
+//     }
+// }
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("FizzBuzz")
+//     } else if (i % 3 === 0) {
+//         console.log("Fizz")
+//     } else if (i % 5 === 0) {
+//         console.log("Buzz")
+//     } else {
+//         console.log(i)
+//     }
+// }
+
+// num = Number(prompt("Enter a number: "))
+// let sum = 0;
+// let temp = num;
+// while (temp > 0) {
+//     let digit = temp % 10;
+//     sum += digit ** 3;
+//     temp = Math.floor(temp / 10);
+// }
+// if (sum === num) {
+//     console.log(num);
+// }
 
 
+// let students = {
+//     "David": 80,
+//     "Vinoth": 77,
+//     "Divya": 88,
+//     "Ishitha": 95,
+//     "Thomas": 68
+// }
+// let totalMarks = 0
+// let studentCount = 0
+
+// for (let student in students) {
+//     totalMarks += students[student]
+//     studentCount++
+// }
+
+// let averageMarks = totalMarks / studentCount
+// console.log(`Average Marks: ${averageMarks}`)
+
+// let grade
+// if (averageMarks >= 90) {
+//     grade = 'A'
+// } else if (averageMarks >= 80) {
+//     grade = 'B'
+// } else if (averageMarks >= 70) {
+//     grade = 'C'
+// } else if (averageMarks >= 60) {
+//     grade = 'D'
+// } else {
+//     grade = 'F'
+// }
+// console.log(`Grade: ${grade}`)
