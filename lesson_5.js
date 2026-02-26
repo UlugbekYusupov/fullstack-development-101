b00621924fefe52585b98326c45e93e14350724d
Fayozbek <fayozbektoychiboyev05@gmail.com>
2026-02-26 13:25:40 +0500

    lesson_5_updated

--- lesson_5.js
+++ lesson_5.js
@@ -50,26 +50,20 @@ function isPrime(n) {
   for (let i = 3; i * i <= n; i += 2) {
     if (n % i === 0) return false;
   }
-
   return true;
 }
-
 
 
 // Challenge 3 - Write a function countDigits(n) that counts the number of digits in a given number.
 
 function countDigits(n) {
   n = Math.abs(n);
-
   if (n === 0) return 1;
-
   let count = 0; 
-
   while (n > 0) {  
     n = Math.floor(n / 10); 
     count++;
   }
-
   return count;
 }
 
@@ -78,10 +72,8 @@ function countDigits(n) {
 
 function isPalindrome(n) {
   if (n < 0) return false;
-
   let original = n;   
   let reversed = 0;    
-
   while (n > 0) {       
     let digit = n % 10;          
     reversed = reversed * 10 + digit;
@@ -93,16 +85,68 @@ function isPalindrome(n) {
 // Challenge 5 - Write a function isArmstrong(n) that checks if a number is an Armstrong number.(An Armstrong number is a number that is equal to the sum of its own digits raised to the power of the number of digits.)
 
 
+function isArmstrong(n) {
+  if (n < 0) return false;
+
+  const s = String(n);
+  const power = s.length;
+
+  let sum = 0;
+  for (const ch of s) {
+    const digit = Number(ch);
+    sum += digit ** power;
+  }
+
+  return sum === n;
+}
 
 // Challenge 6 - Create a function createCounter(start) that returns an object with three methods:
 // increment() ➝ Increases the count
 // decrement() ➝ Decreases the count
 // getCount() ➝ Returns the current count
 
+function createCounter(start) {
+  let count = start; // this is the "private" value
+
+  return {
+    increment() {
+      count++;
+    },
+    decrement() {
+      count--;
+    },
+    getCount() {
+      return count;
+    }
+  };
+}
+
+
 // Challenge 7 – Create a function that order food, the program asks for name, address and what do 	you want to order, then the output should be “Dear (name), your ordered food (food you 	entered) will be ready in 15 minuts and will be delivered to your address(address you 		entered)”
+
+function orderFood() {
+  const name = prompt("Enter your name:");
+  const address = prompt("Enter your address:");
+  const food = prompt("What do you want to order?");
+
+  const message = `Dear ${name}, your ordered food (${food}) will be ready in 15 minutes and will be delivered to your address (${address}).`;
+
+  alert(message);
+  console.log(message); 
+}
+orderFood();
 
 // Challenge 8 - Create a function lazyAdder(a) that returns another function that adds b when called.
 // const add5 = lazyAdder(5); 
 // console.log(add5(10));  - > 15
 // console.log(add5(20));  -> 25
 
+function lazyAdder(a) {
+  return function (b) {
+    return a + b;
+  };
+}
+const add5 = lazyAdder(5);
+
+console.log(add5(10));
+console.log(add5(20));
