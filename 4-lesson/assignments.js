3320c77ca5d0f1218f004fc524f7cb805cb529b8
Muhammad Nur <muhammadnurpdp@gmail.com>
2026-03-03 10:31:18 +0500

    Home Task of 4-lesson

--- 4-lesson/assignments.js
+++ 4-lesson/assignments.js
@@ -13,7 +13,74 @@ function dynamicPrice (count, price) {
 console.log(dynamicPrice(5, 10000))
 
 // 2
-function isStrong (pass) {
+
+function checkPassword(password) {
+   const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+   if (regex.test(password)) {
+      console.log('password is strong');
+   } else {
+      console.log('password is weak');
+
+   }
+}
+checkPassword('1231!Asas')
+
+// 3
+function withdrawATM(amount) {
+  if (amount % 10 !== 0) {
+    return "Error: Amount must be a multiple of $10.";
+  }
+
+  const bills = [100, 50, 20, 10];
+  let result = {};
+  let remaining = amount;
+
+  bills.forEach(bill => {
+    let count = Math.floor(remaining / bill);
+    if (count > 0) {
+      result[`$${bill}`] = count;
+      remaining -= count * bill;
+    }
+  });
+
+  return result;
 }
 
-let password = "password123Ps."
+// 6
+
+function bankAccount(balance) {
+   return {
+      balance: balance,
+      deposit: (e)=>balance + e,
+      withdraw: (e)=>balance - e,
+      viewBalange: ()=>balance,
+   }
+}
+console.log(bankAccount(1000).withdraw(10))
+
+// 7
+function RoleBasedAccessSystem(role) {
+   switch (role) {
+      case 'admin':
+         console.log('can add, edit, delete content');
+         break;
+      case 'editor':
+         console.log('can add, view');
+         break;
+      default:
+         console.log('can only view');
+         break;
+   }
+}
+
+// 9
+
+function dinamicTaxCalculator(income) {
+  if (income > 10000) {
+    console.log("no tax");
+  } else if (income > 5000) {
+    console.log("10% tax");
+  } else {
+    console.log(`20% tax`);
+  }
+}
