2def9485b9439100bb95f4c9fa392a44c4826f37
Sanjarbek <yusanjiy@gmail.com>
2026-03-03 15:16:07 +0500

    lecture 5 assignments

---
+++ lesson 4 (lecture 6)/script.js
@@ -0,0 +1,8 @@
+// Challenge 1
+let num=3 , arr = [1,2,3,4,54,3];
+function show(number,array){
+    for (let i = 0; i < number.length; i++) {
+        console.log(array[i]);
+    }
+}
+show(num,arr);
