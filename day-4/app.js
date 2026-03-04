79ebaa8211e7755570a08432de03bc23be7599cf
sunnatjonpy <webpython9@gmail.com>
2026-03-05 00:34:37 +0500

    Array Homework

--- day-4/app.js
+++ day-4/app.js
@@ -55,6 +55,99 @@ console.log(mapped_original);
 const filtered_array = original.filter((x) => x>50);
 console.log(filtered_array);
 
-// Reducing
 
-    
+
+
+
+
+
+// HomeTasks (Challenges)
+
+
+// Challenge 1
+function first(arr, n) {
+
+  if (!Array.isArray(arr) || arr.length === 0) {
+    return [];
+  }
+
+  if (n === undefined) {
+    return arr[0];
+  }
+
+
+  if (n < 0) {
+    return n;
+  }
+
+  return arr.slice(0, n);
+}
+  
+console.log(first([32,34,56,23]));
+console.log(first([],4));
+console.log(first([32,435,6,543,56,3],4));
+console.log(first([7, 9, 0, -2],6)); 
+console.log(first([7, 9, 0, -2],-3)); 
+
+
+
+// Challenge 2
+
+function last(arr, n) {
+  if (n === undefined) {
+    return arr[-1];
+  }
+
+  else if (arr.length < n) {
+    return arr
+  }
+
+  else if (arr.length >= n) {
+    return arr.slice(-n); 
+  }
+}
+
+console.log(last([7, 9, 0, -2]));
+console.log(last([7, 9, 0, -2],6));
+console.log(last([7, 9, 0, -2],3)); 
+
+
+myColor = ["Red", "Green", "White", "Black"]; 
+console.log(myColor.join(","));
+
+
+// Challenge 4
+nums = "025468";  
+nums_tire = "";
+for (let i = 0; i < nums.length; i++) {
+  nums_tire+=nums[i]
+  if (i !==nums.length-1){
+    nums_tire+="-"
+  }
+  
+}
+
+console.log(nums_tire);
+
+
+
+// Challenge 5
+var arr1 = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+console.log(arr1.sort((a, b) => a - b));
+
+// Challenge 6
+
+// Challenge 7
+
+let string = prompt("Enter any string: ");
+let new_str = "";
+for (let i = 0; i < string.length; i++) {
+  let str = string[i];
+  if (str === str.toUpperCase()){
+    new_str+=str.toLowerCase()
+  }else{
+    new_str+=str.toUpperCase()
+  }
+}
+
+console.log(new_str);
