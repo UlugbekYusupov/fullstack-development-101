0aa99edc08e81ffdb732c46b0007ef008c6ff4de
Sanjarbek <yusanjiy@gmail.com>
2026-03-12 10:21:02 +0500

    Tasks lecture 6

---
+++ lesson 5 (lecture 7).js
@@ -0,0 +1,20 @@
+// challenge 2
+// const cart = [
+//     {id:1,name: "Laptop", price: 900, quantity: 1},
+//     {id:2,name: "Mouse", price: 50, quantity: 2},
+//     {id:3,name: "Keyboard", price: 100, quantity: 1},
+// ]
+//
+// let totalPrice= 0;
+// cart.forEach((item)=>{ totalPrice+=item.price})
+// console.log(totalPrice);
+// totalPrice-=totalPrice/10;
+// console.log("Discrount price "+totalPrice);
+    //
+
+
+const data = [
+    {author: "Dora", likes: 8000000,comment: 118000,shares:72000 },
+    {author: "Sherali", likes: 6700000,comment: 32000,shares:80000 },
+    {author: "Sardor", likes: 911,comment: 700,shares:2 },
+]
