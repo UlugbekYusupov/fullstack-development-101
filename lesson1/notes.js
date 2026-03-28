1ef3735c3530c31a62ca8412590a73fea46f185a
Abdulloh <abdulloh.max2204@gmail.com>
2026-03-28 12:10:25 +0500

    Lesson 1: Introduction

---
+++ lesson1/notes.js
@@ -0,0 +1,48 @@
+// What is the fullstack ?
+
+// Fullstack is works whith:
+
+// Front-end
+// Back-end
+// Database
+// APIs
+
+
+// Client server
+
+// Client sends request
+// Server processes request
+// Server returns response
+
+
+// HTTP and HTTPS
+
+// HTTP = not secure server
+// HTTPS = secure with SSL/TLS
+
+
+// API
+
+// API helps different system to communicate
+
+// Methods:
+
+// -GET
+// -POST
+// -PUT
+// -DELETE
+
+
+
+// PRACTICE
+
+console.log("Fullstack = frontend + backend + database + APIs")
+
+console.log("5" + 5)
+console.log("5" - 5)
+console.log("5" * "2")
+console.log("10" / "2")
+console.log(5 + true)
+console.log("10" - true)
+console.log("5" + true)
+console.log(5 + undefined)
