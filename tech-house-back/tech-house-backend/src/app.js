e913caf00c5b2c3f414e558e574f165592332763
Muhammadyor <musinovmuhammaader@gmail.com>
2026-03-31 21:05:02 +0500

    new project was started

---
+++ tech-house-back/tech-house-backend/src/app.js
@@ -0,0 +1,19 @@
+import express from "express"
+import cors from "cors"
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/", (req, res) => {
+    res.json({
+        message: "Api is running correctly!"
+    })
+})
+
+
+
+export default app;
+
+
